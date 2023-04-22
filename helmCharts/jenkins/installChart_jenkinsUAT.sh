d1b947bd6309bdf9c8966b812292bb0654228644
dimitrisdol <jimmdologlou@gmail.com>
2023-04-22 18:02:53 +0300

    Added persistent volume inside helmChart

--- helmCharts/jenkins/installChart_jenkinsUAT.sh
+++ helmCharts/jenkins/installChart_jenkinsUAT.sh
@@ -3,9 +3,6 @@
 ENV_ID=uat	
 VERSION=4.3.22
 
-# Install Persistent Volume Claim for cluster
-kubectl apply -f volume.yaml
-
 # Install Jenkins
 
 helm upgrade --install jenkins --values common_values.yaml --values ${ENV_ID}_values.yaml --atomic -n jenkins --create-namespace --timeout 5m0s jenkins-${VERSION}.tgz --debug
