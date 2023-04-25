03c423bfb5e45e6bf84e10dc7de08a197d92cdcf
dimitrisdol <jimmdologlou@gmail.com>
2023-04-25 13:44:50 +0300

    Testing first deployment

--- helmCharts/jenkins/installChart_jenkinsUAT.sh
+++ helmCharts/jenkins/installChart_jenkinsUAT.sh
@@ -5,6 +5,6 @@ VERSION=4.3.22
 
 # Install Jenkins
 
-helm upgrade --install jenkins --values common_values.yaml --values ${ENV_ID}_values.yaml --atomic -n jenkins --create-namespace --timeout 5m0s jenkins-${VERSION}.tgz 
+helm upgrade --install jenkins --values common_values.yaml --values ${ENV_ID}_values.yaml --atomic -n jenkins --create-namespace --timeout 5m0s jenkins-${VERSION}.tgz  --debug
 
 
