768ae5da00efda3f1db1831dcbd3a07a017eb00e
dimitrisdol <jimmdologlou@gmail.com>
2023-04-28 17:33:51 +0300

    Added more timeout duration

--- helmCharts/nexus/installChart_nexusUAT.sh
+++ helmCharts/nexus/installChart_nexusUAT.sh
@@ -5,4 +5,4 @@ VERSION=52.0.0
 
 # Install Jenkins
 
-helm upgrade --install nexus --values common_values.yaml --values ${ENV_ID}_values.yaml --atomic -n nexus --create-namespace --timeout 5m0s nexus-repository-manager-${VERSION}.tgz  --debug
+helm upgrade --install nexus --values common_values.yaml --values ${ENV_ID}_values.yaml --atomic -n nexus --create-namespace --timeout 10m0s nexus-repository-manager-${VERSION}.tgz  --debug
